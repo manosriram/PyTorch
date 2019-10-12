5c246cd2f2fa9eb9a4711ba8ea4a81f5dd8181aa
Mano Sriram <mano.sriram0@gmail.com>
2019-10-12 20:32:57 +0530

    Add log reg book.

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -4,16 +4,18 @@ import matplotlib.pyplot as plot
 from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.datasets import load_digits
+from sklearn.datasets import load_digits, fetch_openml
 
 digits = load_digits();
 logReg = LogisticRegression()
 
+# xTrain, xTest, yTrain, yTest = train_test_split(digits.data, digits.target, test_size=0.25, random_state=0)
 
-xTrain, xTest, yTrain, yTest = train_test_split(digits.data, digits.target, test_size = 0.25, random_state = 0)
+# logReg.fit(xTrain, yTrain)
 
-logReg.fit(xTrain, yTrain)
+print(X)
+# te = xTest[0].reshape(1,-1)
 
-c = logReg.predict(xTest)
+# c = logReg.predict(te)
 
-print(c)
+# print(c)
